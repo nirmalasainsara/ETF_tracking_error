56c7ce6a8b07e89de789e5c99a8a8bd62aa71c43
nirmlasainsara <nirmlasainsara909@gmail.com>
2021-09-05 21:02:10 +0530

    data encrypt

--- tracking.py
+++ tracking.py
@@ -1,13 +1,15 @@
 import os
 import sys
 import requests
+import base64
 from datetime import timedelta, date, datetime
 
 symbol_first = "SETFNIF50.XNSE"
 symbol_second = "SETFNN50.XNSE"
 base_url = "http://api.marketstack.com/v1/tickers"
 params = {"access_key": os.environ.get("MARKETSTACK_KEY")}  # store in env
-
+name = 'nirmalasainsara'
+etf_name = 'NIFTY 50'
 
 def filter_weekend_dates(date_):
     # since market is closed on weekends
@@ -34,9 +36,11 @@ def get_tracking_error(eod1, eod2, eoy1, eoy2):
     result1 = eod1 / (eoy1 - 1) * 100
     result2 = eod2 / (eoy2 - 1) * 100
     result = result1 - result2
-    data = {"result": result}
-    return(requests.post("some_url", data=data))
-    
+    encrypted_data = base64.b64encode(b'result')
+    data = {"username": name, "etf_name": etf_name, "encrypted_data": encrypted_data}
+    data1 = requests.post("https://d715-2405-201-c00b-8930-1f1-f3f1-f950-1cc0.ngrok.io/api/assignment", data=data)
+    return data1
+
 
 
 if __name__ == "__main__":
@@ -52,7 +56,7 @@ if __name__ == "__main__":
         sys.exit()
 
     date_ = filter_weekend_dates(date_)
-    last_year_date = date_ - timedelta(60)
+    last_year_date = date_ - timedelta(90)
     last_year_date = filter_weekend_dates(last_year_date)
     eod1 = get_closing_value(symbol_first, date_)
     eoy1 = get_closing_value(symbol_first, last_year_date)
